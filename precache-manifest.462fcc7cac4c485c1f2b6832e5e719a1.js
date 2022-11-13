c7ab9de1dfff0dd41e918e78a3bc18872fc1e020
chris <changchiyou@gmail.com>
2022-11-13 18:55:59 +0800

    Updates

---
+++ precache-manifest.462fcc7cac4c485c1f2b6832e5e719a1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf97d6b18eb7cb922613",
+    "url": "/WildRiftGoldEfficiency/static/js/main.bf97d6b1.chunk.js"
+  },
+  {
+    "revision": "5a7e676a718643eca944",
+    "url": "/WildRiftGoldEfficiency/static/js/1.5a7e676a.chunk.js"
+  },
+  {
+    "revision": "f10042804f5067c0f93a",
+    "url": "/WildRiftGoldEfficiency/static/js/runtime~main.f1004280.js"
+  },
+  {
+    "revision": "24719a2e955a45bcea38a11f369e9a14",
+    "url": "/WildRiftGoldEfficiency/index.html"
+  }
+];
