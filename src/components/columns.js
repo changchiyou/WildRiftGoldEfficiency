e1410407a8516ff3071242b7a92da6765ebf5416
Tommy <tommy93211@gmail.com>
2022-11-13 20:06:02 +0800

    react table

---
+++ src/components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },  
+    {
+        Header: 'First name',
+        accessor: 'first_name'
+    },  
+    {
+        Header: 'Email',
+        accessor: 'email'
+    },  
+    {
+        Header: 'Gender',
+        accessor: 'gender'
+    },  
+    {
+        Header: 'IP Address',
+        accessor: 'ip_address'
+    }
+]
